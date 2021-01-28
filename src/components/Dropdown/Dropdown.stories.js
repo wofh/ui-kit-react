755b0d51186d413b3e94eec2adf557908b0b0b83
Stefano Marra <stefano.marra1987@gmail.com>
2021-01-28 21:07:52 +0100

    Update dropdown storybook preview

--- src/components/Dropdown/Dropdown.stories.js
+++ src/components/Dropdown/Dropdown.stories.js
@@ -11,54 +11,60 @@ export default {
 const dropdownContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
 
 export const Default = () => (
-   <Box pad={'large'} align={'center'} style={{ height: '300px' }}>
-      <Box style={{ display: 'inline-flex' }}>
+   <Box pad={'large'} align={'center'} style={{ height: '300px', display: 'flex', alignItems: 'center' }}>
+      <Box style={{ margin: '0 auto' }}>
          <Dropdown trigger={<Button label={'Click to open'} use={'primary'} />} background={'lighter'}>{dropdownContent}</Dropdown>
       </Box>
    </Box>
 );
 
 export const Position = () => (
-   <Box pad={'large'} align={'center'} style={{ height: '300px' }}>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Bottom'} />}>{dropdownContent}</Dropdown>
-      </Box>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Top'} />} position={'top'}>{dropdownContent}</Dropdown>
-      </Box>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Left'} />} position={'left'}>{dropdownContent}</Dropdown>
-      </Box>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Right'} />} position={'right'}>{dropdownContent}</Dropdown>
+   <Box pad={'large'} align={'center'} style={{ height: '300px', display: 'flex', alignItems: 'center' }}>
+      <Box style={{ margin: '0 auto' }}>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Bottom'} />}>{dropdownContent}</Dropdown>
+         </Box>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Top'} />} position={'top'}>{dropdownContent}</Dropdown>
+         </Box>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Left'} />} position={'left'}>{dropdownContent}</Dropdown>
+         </Box>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Right'} />} position={'right'}>{dropdownContent}</Dropdown>
+         </Box>
       </Box>
    </Box>
 );
 
 export const Align = () => (
-   <Box pad={'large'} align={'center'} style={{ height: '300px' }}>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Left'} />} align={'left'}>{dropdownContent}</Dropdown>
-      </Box>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Center'} />} align={'center'}>{dropdownContent}</Dropdown>
-      </Box>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Right'} />} align={'right'}>{dropdownContent}</Dropdown>
+   <Box pad={'large'} align={'center'} style={{ height: '300px', display: 'flex', alignItems: 'center' }}>
+      <Box style={{ margin: '0 auto' }}>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Left'} />} align={'left'}>{dropdownContent}</Dropdown>
+         </Box>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Center'} />} align={'center'}>{dropdownContent}</Dropdown>
+         </Box>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Right'} />} align={'right'}>{dropdownContent}</Dropdown>
+         </Box>
       </Box>
    </Box>
 );
 
 export const Width = () => (
-   <Box pad={'large'} align={'center'} style={{ height: '300px' }}>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'240'} />} w={240} background={'lighter'}>{dropdownContent}</Dropdown>
-      </Box>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'280px'} />} w={'280px'} background={'lighter'}>{dropdownContent}</Dropdown>
-      </Box>
-      <Box style={{ display: 'inline-flex' }}>
-         <Dropdown trigger={<Button label={'Min / Max'} />} w={{ min: 180, max: 280 }} background={'lighter'}>{dropdownContent}</Dropdown>
+   <Box pad={'large'} align={'center'} style={{ height: '300px', display: 'flex', alignItems: 'center' }}>
+      <Box style={{ margin: '0 auto' }}>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'240'} />} w={240} background={'lighter'}>{dropdownContent}</Dropdown>
+         </Box>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'320px'} />} w={'320px'} background={'lighter'}>{dropdownContent}</Dropdown>
+         </Box>
+         <Box style={{ display: 'inline-flex' }}>
+            <Dropdown trigger={<Button label={'Min / Max'} />} w={{ min: 180, max: 280 }} background={'lighter'}>{dropdownContent}</Dropdown>
+         </Box>
       </Box>
    </Box>
 );
