8ccb1f227ff6bebc33ddd884dac6b7510adb814e
Stefano Marra <stefano.marra1987@gmail.com>
2021-02-11 20:18:16 +0100

    Update select component

--- src/components/Field/Select.js
+++ src/components/Field/Select.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import { useState, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-import { useSelect } from 'react-select-search';
-import { Button } from '../Button';
-import { Dropdown } from '../Dropdown';
 import { Icon } from '../Icon';
-import { Input } from './Input';
 import { color, spacing, typography } from '../../shared/styles';
-import { hex2rgba } from '../../shared/mixins'
+import { hex2rgba } from '../../shared/mixins';
 
 const StyledIcon = styled.span`
    position: absolute;
@@ -15,84 +11,87 @@ const StyledIcon = styled.span`
    width: 36px;
    text-align: center;
    color: ${color.mediumdark};
-`
+`;
 
-const StyledOption = styled.option``
+const StyledOption = styled.option``;
 
 const StyledSelect = styled.select`
-    width: 100%;
-    border: none;
-    height: ${typography.size.m2 * 2}px;
-    background-color: transparent;
-`
+   width: 100%;
+   border: none;
+   height: ${typography.size.m2 * 2}px;
+   background-color: transparent;
+`;
 
 const StyledInput = styled.div`
-    position: relative;
+   position: relative;
 
-    display: block;
-    width: 100%;
-    border: none;
-    outline: none;
+   display: block;
+   width: 100%;
+   border: none;
+   outline: none;
 
-    font-size: ${typography.size.s2}px;
-    line-height: ${typography.size.m2 * 2}px;
-    box-shadow: inset 0 0 0 1px ${color.medium};
-    border-radius: ${spacing.borderRadius.default}px;
-    padding: 0 ${typography.size.m1}px;
+   font-size: ${typography.size.s2}px;
+   line-height: ${typography.size.m2 * 2}px;
+   box-shadow: inset 0 0 0 1px ${color.medium};
+   border-radius: ${spacing.borderRadius.default}px;
+   padding: 0 ${typography.size.m1}px;
 
-    ${props => props.plain && css`
-        padding: 0;
-        padding-right: ${typography.size.s1}px;
+   ${(props) =>
+      props.plain &&
+      css`
+         padding: 0;
+         padding-right: ${typography.size.s1}px;
 
-        ${StyledSelect} {
+         ${StyledSelect} {
             padding: 0 ${typography.size.m1}px;
-        }
-    `}
+         }
+      `}
 
-    ${StyledIcon} {
-        top: 50%;
-        transform: translateY(-50%);
+   ${StyledIcon} {
+      top: 50%;
+      transform: translateY(-50%);
 
-        &:first-child {
-            left: 0;
-        }
+      &:first-child {
+         left: 0;
+      }
 
-        &:last-child {
-            right: 0;
-        }
-    }
-`
+      &:last-child {
+         right: 0;
+      }
+   }
+`;
 
 const StyledItemButton = styled.button`
-    width: 100%;
-    border: none;
-    background-color: transparent;
-    padding: ${spacing.padding.medium * 0.5}px ${spacing.padding.medium}px;
-    margin: ${spacing.margin.xsmall * 0.5}px 0;
-    text-align: left;
-    cursor: pointer;
-    border-radius: ${spacing.borderRadius.default}px;
+   width: 100%;
+   border: none;
+   background-color: transparent;
+   padding: ${spacing.padding.medium * 0.5}px ${spacing.padding.medium}px;
+   margin: ${spacing.margin.xsmall * 0.5}px 0;
+   text-align: left;
+   cursor: pointer;
+   border-radius: ${spacing.borderRadius.default}px;
 
-    &:hover {
-        background-color: ${hex2rgba(color.primary, 0.1)};
-    }
+   &:hover {
+      background-color: ${hex2rgba(color.primary, 0.1)};
+   }
 
-    ${props => props.active && css`
-        background-color: ${color.primary};
-        color: #FFF;
+   ${(props) =>
+      props.active &&
+      css`
+         background-color: ${color.primary};
+         color: #fff;
 
-        &:hover {
+         &:hover {
             background-color: ${hex2rgba(color.primary, 0.9)};
-        }
-    `}
+         }
+      `}
 
-
-    &:disabled,
+   &:disabled,
     &:disabled:hover {
-        background-color: transparent;
-        cursor: default;
-    }
-`
+      background-color: transparent;
+      cursor: default;
+   }
+`;
 
 const StyledWrapper = styled.div`
     position: relative;
@@ -117,168 +116,301 @@ const StyledWrapper = styled.div`
     }
 
     ${StyledInput} {
-        ${(props) => (!props.error && !props.success) && css`
+        ${(props) =>
+           !props.error &&
+           !props.success &&
+           css`
+              ${props.focus &&
+              css`
+                 box-shadow: inset 0 0 0 1px ${color.primary};
+              `}
+           `}
 
-            ${props.focus && css`
-                box-shadow: inset 0 0 0 1px ${color.primary};
-            `}
-        `}
+        ${(props) =>
+           props.error &&
+           css`
+              color: ${color.danger};
+              box-shadow: inset 0 0 0 1px ${color.danger};
+           `}
 
-        ${(props) => props.error && css`
-            color: ${color.danger};
-            box-shadow: inset 0 0 0 1px ${color.danger};
-        `}
-
-        ${(props) => props.success && css`
-            color: ${color.success};
-            box-shadow: inset 0 0 0 1px ${color.success};
-        `}
+        ${(props) =>
+           props.success &&
+           css`
+              color: ${color.success};
+              box-shadow: inset 0 0 0 1px ${color.success};
+           `}
     }
-`
+`;
 
-export const Select = ({ onChange, plain, open, options, multiSelect, searchable, ...props }) => {
-    const [snapshot, valueProps, optionProps] = useSelect({
-        options,
-        multiple: multiSelect,
-        search: searchable,
-        printOptions: open?'always':'auto',
-        onChange: onChange,
-        ...props,
+const isOption = (option) => {
+   return option !== null && typeof option === 'object' && 'value' in option && 'name' in option;
+};
 
-        // https://github.com/tbleckert/react-select-search
-        // getOptions: (query) => {}
-    });
+const getDisplayValue = (value) => {
+   if (Array.isArray(value)) {
+      return value.map((o) => isOption(o) && o.name).join(', ');
+   }
 
-    // console.log('props', props)
+   return isOption(value) ? value.name : '';
+};
 
-    // console.log('snapshot', snapshot)
-    // console.log('valueProps', valueProps)
-    // console.log('optionProps', optionProps)
+const getOption = (value, options) => {
+   if (isOption(value)) {
+      return value;
+   }
 
-    const getArrowIcon = () =>  (
-        <StyledIcon>
-            <Icon icon={'arrowdown'} />
-        </StyledIcon>
-    )
+   const newValue = value === null && options.length ? options[0].value : value;
 
-    const getIconLeft = () =>
-        props.iconLeft ? (
-            <StyledIcon>
-                <Icon icon={props.iconLeft} />
-            </StyledIcon>
-        ) : null;
+   // eslint-disable-next-line eqeqeq
+   return options.find((o) => o.value == newValue);
+};
 
-    const getIconRight = () =>
-        props.iconRight ? (
-            <StyledIcon>
-                <Icon icon={props.iconRight} />
-            </StyledIcon>
-        ) : (plain ? null : getArrowIcon());
+const getOptions = (value, oldValue, options, multiple) => {
+   const newOption = getOption(value, options);
 
-    const isOptionActive = (option) => {
+   if (!multiple) {
+      return newOption || oldValue;
+   }
 
-        if (Array.isArray(snapshot.value)) {
-            return snapshot.value.includes(option.value)
-        }
+   const newOptions = valueToArray(oldValue)
+      .map((o) => getOption(o, options))
+      .filter((o) => o !== null && o !== undefined);
 
-        return snapshot.value === option.value
-    }
+   if (!newOption) {
+      return newOptions;
+   }
 
-    const getOptions = () => {
+   // eslint-disable-next-line eqeqeq
+   const optionIndex = newOptions.findIndex((o) => o.value == newOption.value);
 
-        if ( plain ) {
-            return [{ name: props.placeholder, disabled: true, selected: true }].concat(options).map(({ name, value, disabled, ...option}) => (
-                <StyledOption key={name} value={value} disabled={disabled||false} {...option}>{name}</StyledOption>
-            ))
-        }
+   if (optionIndex >= 0) {
+      newOptions.splice(optionIndex, 1);
+   } else {
+      newOptions.push(newOption);
+   }
 
-        if ( snapshot.focus || open ) {
-            return (
-                <ul>
-                    <li>
-                        <StyledItemButton value={''} disabled={true}>{props.placeholder}</StyledItemButton>
-                    </li>
-                    {snapshot.options.map((option) => (
-                        <li key={option.name}>
-                            <StyledItemButton active={isOptionActive(option)} {...optionProps} value={option.value} disabled={option.disabled||false}>{option.name}</StyledItemButton>
-                        </li>
-                    ))}
-                </ul>
-            )
-        }
+   return newOptions;
+};
 
-        return null
-    }
+const getValue = (option) => {
+   if (!option) {
+      return null;
+   }
 
-    if (plain) {
-        return (
-            <StyledWrapper {...props}>
-                <StyledInput plain={plain} {...props}>
-                    {getIconLeft()}
-                    <StyledSelect {...props} defaultValue={props.value} onChange={onChange}>
-                        {getOptions()}
-                    </StyledSelect>
-                    {getIconRight()}
-                </StyledInput>
-            </StyledWrapper>
-        )
-    }
+   return isOption(option) ? option.value : null;
+};
 
-    return (
-        <StyledWrapper {...props} {...snapshot}>
-            <StyledInput {...valueProps} {...props}>
-                {getIconLeft()}
-                <span>{snapshot.displayValue || props.placeholder || ''}</span>
-                {getIconRight()}
+const valueToArray = (value) => {
+   return !Array.isArray(value) ? [value] : value;
+};
+
+const getValues = (options) => {
+   if (Array.isArray(options)) {
+      return options.map((o) => getValue(o)).filter((v) => v !== null);
+   }
+
+   return getValue(options);
+};
+
+export const Select = ({
+   closeOnSelect,
+   plain,
+   open,
+   options,
+   multiSelect,
+   searchable,
+   onChange = () => {},
+   onFocus = () => {},
+   onBlur = () => {},
+   ...props
+}) => {
+   const ref = useRef(null);
+   const [value, setValue] = useState(multiSelect ? [] : null);
+   const [focus, setFocus] = useState(false);
+   // const [search, setSearch] = useState('');
+
+   const onSelect = useCallback(
+      (newValue) => {
+         const newOption = getOptions(
+            newValue,
+            value,
+            Array.isArray(value) ? [...value, ...options] : options,
+            multiSelect
+         );
+
+         setValue(newOption);
+         onChange(getValues(newOption), newOption);
+
+         if (closeOnSelect && ref) {
+            ref.current.blur();
+         }
+      },
+      [closeOnSelect, multiSelect, onChange, value, options]
+   );
+
+   const onMouseDown = useCallback(
+      (e) => {
+         e.preventDefault();
+         onSelect(e.currentTarget.value);
+      },
+      [onSelect]
+   );
+
+   const handleFocus = useCallback(
+      (e) => {
+         setFocus(true);
+         onFocus(e);
+      },
+      [onFocus]
+   );
+
+   const handleBlur = useCallback(
+      (e) => {
+         setFocus(false);
+         // setSearch('');
+         onBlur(e);
+      },
+      [onBlur]
+   );
+
+   const isOptionActive = (option) => {
+      if (Array.isArray(getValues(value))) {
+         return getValues(value).includes(option.value);
+      }
+
+      return getValues(value) === option.value;
+   };
+
+   const renderArrowIcon = () => (
+      <StyledIcon>
+         <Icon icon={'arrowdown'} />
+      </StyledIcon>
+   );
+
+   const renderIconLeft = () =>
+      props.iconLeft ? (
+         <StyledIcon>
+            <Icon icon={props.iconLeft} />
+         </StyledIcon>
+      ) : null;
+
+   const renderIconRight = () =>
+      props.iconRight ? (
+         <StyledIcon>
+            <Icon icon={props.iconRight} />
+         </StyledIcon>
+      ) : plain ? null : (
+         renderArrowIcon()
+      );
+
+   const renderOptions = () => {
+      if (plain) {
+         return [{ name: props.placeholder, disabled: true, selected: true }]
+            .concat(options)
+            .map(({ name, value, disabled, ...option }) => (
+               <StyledOption key={name} value={value} disabled={disabled || false} {...option}>
+                  {name}
+               </StyledOption>
+            ));
+      }
+
+      if (focus || open) {
+         return (
+            <ul>
+               <li>
+                  <StyledItemButton value={''} disabled={true}>
+                     {props.placeholder}
+                  </StyledItemButton>
+               </li>
+               {options.map((option) => (
+                  <li key={option.name}>
+                     <StyledItemButton
+                        active={isOptionActive(option)}
+                        value={option.value}
+                        disabled={option.disabled || false}
+                        onMouseDown={onMouseDown}
+                        tabIndex={'-1'}
+                     >
+                        {option.name}
+                     </StyledItemButton>
+                  </li>
+               ))}
+            </ul>
+         );
+      }
+
+      return null;
+   };
+
+   if (plain) {
+      return (
+         <StyledWrapper {...props}>
+            <StyledInput plain={plain} {...props}>
+               {renderIconLeft()}
+               <StyledSelect {...props} defaultValue={props.value} onChange={onChange}>
+                  {renderOptions()}
+               </StyledSelect>
+               {renderIconRight()}
             </StyledInput>
-            {getOptions()}
-        </StyledWrapper>
-    )
-}
+         </StyledWrapper>
+      );
+   }
 
+   return (
+      <StyledWrapper {...props} focus={focus}>
+         <StyledInput {...props} ref={ref} tabIndex={0} onFocus={handleFocus} onBlur={handleBlur}>
+            {renderIconLeft()}
+            <span>{getDisplayValue(value) || props.placeholder || ''}</span>
+            {renderIconRight()}
+         </StyledInput>
+         {renderOptions()}
+      </StyledWrapper>
+   );
+};
+
+// Based on: https://github.com/tbleckert/react-select-search/blob/main/src/useSelect.js
 Select.propTypes = {
+   /**
+    * The options the user can select from
+    */
+   options: PropTypes.arrayOf(
+      PropTypes.shape({
+         value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+         name: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+         disabled: PropTypes.bool,
+      })
+   ).isRequired,
 
-    /**
-     * The options the user can select from
-     */
-    options: PropTypes.arrayOf(
-        PropTypes.shape({
-            value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-            name: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-            disabled: PropTypes.bool,
-        })
-    ).isRequired,
+   /**
+    * Close the selectbox on select
+    */
+   closeOnSelect: PropTypes.bool,
 
-    /**
-     * Close the selectbox on select
-     */
-    closeOnSelect: PropTypes.bool,
+   /**
+    * If `true` enables multiselect
+    */
+   multiSelect: PropTypes.bool,
 
-    /**
-     * If `true` enables multiselect
-     */
-    multiSelect: PropTypes.bool,
+   /**
+    * If `true` enables search
+    */
+   // searchable: PropTypes.bool,
 
-    /**
-     * If `true` enables search
-     */
-    // searchable: PropTypes.bool,
+   /**
+    * If `true` options will be rendered open
+    */
+   open: PropTypes.bool,
 
-    /**
-     * If `true` options will be rendered open
-     */
-    open: PropTypes.bool,
-
-    /**
-     * If `true` a plain select with minimal styling/feature will be rendered
-     */
-    plain: PropTypes.bool,
+   /**
+    * If `true` a plain select with minimal styling/feature will be rendered
+    */
+   plain: PropTypes.bool,
 };
 
 Select.defaultProps = {
-    multiSelect: false,
-    // searchable: false,
-    closeOnSelect: true,
-    open: false,
-    plain: false,
+   multiSelect: false,
+   // searchable: false,
+   closeOnSelect: true,
+   open: false,
+   plain: false,
 };
