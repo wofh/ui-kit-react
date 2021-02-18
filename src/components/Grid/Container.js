d7bb799de764ac1225cde197ebf327e613677439
Stefano Marra <stefano.marra1987@gmail.com>
2021-02-18 15:01:34 +0100

    Fix proptype issue

--- src/components/Grid/Container.js
+++ src/components/Grid/Container.js
@@ -6,25 +6,28 @@ const StyledContainer = styled.div`
    box-sizing: border-box;
    display: block;
    width: 100%;
-   max-width: ${props => (typeof props.maxWidth == 'number') ? (props.maxWidth + 'px') : props.maxWidth};
+   max-width: ${(props) =>
+      typeof props.maxWidth == 'number' ? props.maxWidth + 'px' : props.maxWidth};
 
-   margin: ${props => (props.align == 'left') ? '0 auto 0 0' : (props.align == 'right' ? '0 0 0 auto' : '0 auto')};
+   margin: ${(props) =>
+      props.align == 'left' ? '0 auto 0 0' : props.align == 'right' ? '0 0 0 auto' : '0 auto'};
 
-   ${props => (typeof props.spaceAfter !== 'undefined') && css`margin-bottom: ${props.spaceAfter}px;`}
+   ${(props) =>
+      typeof props.spaceAfter !== 'undefined' &&
+      css`
+         margin-bottom: ${props.spaceAfter}px;
+      `}
 `;
 
 export const Container = ({ children, ...props }) => {
-   return (
-      <StyledContainer {...props}>{children}</StyledContainer>
-   )
-}
+   return <StyledContainer {...props}>{children}</StyledContainer>;
+};
 
 Container.propTypes = {
-
    /**
     * Defines the max width of the container. It can be a number (e.g. `600`) of a string (e.g. `800px` or `100%`)
     */
-   maxWidth: PropTypes.oneOfType(PropTypes.number, PropTypes.string),
+   maxWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
    /**
     * Horizontal arrangement of the container.
