28e16bf73107bd75722e5cbcb3fc16fb6459270e
Stefano Marra <stefano.marra1987@gmail.com>
2021-02-25 17:41:05 +0100

    Fix sidebar width

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -15,10 +15,12 @@ const StyledItemIcon = styled.span`
    width: ${spacing.padding.medium * 2}px;
    line-height: ${typography.size.l1 * 2}px;
    display: inline-block;
-   ${props => props.left && css`
-      text-align: right;
-      color: ${color.darker} !important;
-   `}
+   ${(props) =>
+      props.left &&
+      css`
+         text-align: right;
+         color: ${color.darker} !important;
+      `}
 
    & > * {
       margin-top: -2px;
@@ -28,9 +30,12 @@ const StyledItemIcon = styled.span`
 const StyledItemLabel = styled.span`
    flex: 1 1 0%;
    line-height: ${typography.size.l1 * 2}px;
+   text-overflow: ellipsis;
+   overflow: hidden;
 `;
 
 const StyledSubItem = styled.div`
+   white-space: nowrap;
 `;
 
 const StyledItem = styled.button`
@@ -43,7 +48,7 @@ const StyledItem = styled.button`
    cursor: pointer;
    padding: 0;
    text-align: left;
-   color: ${props => props.active ? color.primary : color.darker};
+   color: ${(props) => (props.active ? color.primary : color.darker)};
    font-size: ${typography.size.s3}px;
    line-height: ${typography.size.m1 * 2}px;
    font-weight: ${typography.weight.medium};
@@ -52,38 +57,43 @@ const StyledItem = styled.button`
       opacity: 0.8;
    }
 
-   ${props => (props.active && !props.isSubItem) && css`
-      &:before {
-         content: "";
-         position: absolute;
-         width: ${spacing.borderRadius.xsmall}px;
-         height: ${typography.size.m1 * 1.5}px;
-         background-color: ${color.primary};
-         top: 50%;
-         transform: translateY(-50%);
-         left: -20px;
-         border-radius: 0 ${spacing.borderRadius.xsmall}px ${spacing.borderRadius.xsmall}px 0;
-      }
+   ${(props) =>
+      props.active &&
+      !props.isSubItem &&
+      css`
+         &:before {
+            content: '';
+            position: absolute;
+            width: ${spacing.borderRadius.xsmall}px;
+            height: ${typography.size.m1 * 1.5}px;
+            background-color: ${color.primary};
+            top: 50%;
+            transform: translateY(-50%);
+            left: -20px;
+            border-radius: 0 ${spacing.borderRadius.xsmall}px ${spacing.borderRadius.xsmall}px 0;
+         }
 
-      &:after {
-         content: "";
-         position: absolute;
-         background-color: ${hex2rgba(color.primary, 0.1)};
-         top: 0;
-         bottom: 0;
-         left: -20px;
-         right: -20px;
-      }
-   `};
+         &:after {
+            content: '';
+            position: absolute;
+            background-color: ${hex2rgba(color.primary, 0.1)};
+            top: 0;
+            bottom: 0;
+            left: -20px;
+            right: -20px;
+         }
+      `};
 
    & + ${StyledSubItem} {
-      ${props => !props.active && 'display: none;'}
+      ${(props) => !props.active && 'display: none;'}
    }
 
    ${StyledItemIcon} {
-      ${props => props.active && css`
-         color: ${color.primary};
-      `}
+      ${(props) =>
+         props.active &&
+         css`
+            color: ${color.primary};
+         `}
    }
 `;
 
@@ -104,14 +114,14 @@ const StyledSidebar = styled.div`
    display: flex;
    flex-direction: column;
    max-width: 100%;
-   ${props => !props.collapsed && 'min-width: 250px;'}
+   ${(props) => !props.collapsed && 'min-width: 250px;'}
    min-height: 100%;
    padding: ${spacing.padding.medium}px;
-   background-color: ${props => getBackgroundColor(props)};
+   background-color: ${(props) => getBackgroundColor(props)};
    box-shadow: 0 0 0 1px ${color.medium};
 
    ${StyledItem} {
-      ${props => props.collapsed && 'text-align: center;'}
+      ${(props) => props.collapsed && 'text-align: center;'}
    }
 `;
 
@@ -139,106 +149,117 @@ const StyledSidebarWrapper = styled.div`
  * An item object can have a sub set of items defined in with the key `items`.
  */
 export const Sidebar = ({ items, activePath, footer, children, header, collapsed, ...props }) => {
-   const theme = useTheme()
-   const propsWithTheme = { theme, ...props }
+   const theme = useTheme();
+   const propsWithTheme = { theme, ...props };
 
-   const [activeItem, setActiveItem] = useState(null)
-   const [activeParentItem, setActiveParentItem] = useState(null)
+   const [activeItem, setActiveItem] = useState(null);
+   const [activeParentItem, setActiveParentItem] = useState(null);
 
    const isActiveItemParent = (item) => {
-
       if (item && item.items) {
-         return item.items.find(i => i.path && i.path === activePath) || item === activeParentItem
+         return (
+            item.items.find((i) => i.path && i.path === activePath) || item === activeParentItem
+         );
       }
 
       if (item && item.path && activePath) {
-         return activePath === item.path
+         return activePath === item.path;
       }
 
-      return activeParentItem === item
-   }
+      return activeParentItem === item;
+   };
 
    const isActiveItem = (item) => {
       if (item && item.path && activePath) {
-         return activePath === item.path
+         return activePath === item.path;
       }
 
-      return activeItem === item
-   }
+      return activeItem === item;
+   };
 
    const isSubItemActive = (item) => {
-      return items.filter((i => i == item)).length === 0
-   }
+      return items.filter((i) => i == item).length === 0;
+   };
 
    const handleClick = (item, parentItem) => {
-
       if (!activePath) {
-
-         if (activeParentItem && (activeParentItem.label === item.label)) {
-            setActiveItem(null)
-            setActiveParentItem(null)
-         }
-         else {
-            setActiveItem(activeItem===item?null:item)
-            setActiveParentItem(parentItem?parentItem:null)
+         if (activeParentItem && activeParentItem.label === item.label) {
+            setActiveItem(null);
+            setActiveParentItem(null);
+         } else {
+            setActiveItem(activeItem === item ? null : item);
+            setActiveParentItem(parentItem ? parentItem : null);
 
             if (item.items && item.items.length) {
-               handleClick(item.items[0], item)
+               handleClick(item.items[0], item);
             }
          }
       }
 
-      if (item.onClick) item.onClick(item)
-   }
+      if (item.onClick) item.onClick(item);
+   };
 
-   const getItemIcon = icon => (
-      <StyledItemIcon>
-         {icon ? <Icon icon={icon} /> : null}
-      </StyledItemIcon>
-   )
+   const getItemIcon = (icon) => (
+      <StyledItemIcon>{icon ? <Icon icon={icon} /> : null}</StyledItemIcon>
+   );
 
-   const getItemIconArrow = item => (
-      <StyledItemIcon left>
-         {item.items && item.items.length ? <Icon icon={(activeParentItem || activeItem) === item ? 'ChevronUp' :'ChevronDown'} /> : null}
-      </StyledItemIcon>
-   )
+   const getItemIconArrow = (item) => {
+      if (item.items && item.items.length) {
+         return (
+            <StyledItemIcon left>
+               <Icon
+                  icon={(activeParentItem || activeItem) === item ? 'ChevronUp' : 'ChevronDown'}
+               />
+            </StyledItemIcon>
+         );
+      }
+      return null;
+   };
 
-   const getItemLabel = label => label ? (
-      <StyledItemLabel>{label}</StyledItemLabel>
-   ) : null
+   const getItemLabel = (label) => (label ? <StyledItemLabel>{label}</StyledItemLabel> : null);
 
    const getItem = (item, index, parentItem = null) => {
       return (
          <div key={index}>
-            <StyledItem isSubItem={isSubItemActive(item)} active={isActiveItem(item) || isActiveItemParent(item)} onClick={() => handleClick(item, parentItem)}>
-               {getItemIcon(item.icon||null)}
-               {!collapsed && getItemLabel(item.label||null)}
+            <StyledItem
+               isSubItem={isSubItemActive(item)}
+               active={isActiveItem(item) || isActiveItemParent(item)}
+               onClick={() => handleClick(item, parentItem)}
+            >
+               {getItemIcon(item.icon || null)}
+               {!collapsed && getItemLabel(item.label || null)}
                {!collapsed && getItemIconArrow(item)}
             </StyledItem>
-            {item.items ? <StyledSubItem>{getItems(item.items, item)}</StyledSubItem>:null}
+            {item.items ? <StyledSubItem>{getItems(item.items, item)}</StyledSubItem> : null}
          </div>
-      )
-   }
+      );
+   };
 
    const getItems = (items, parentItem) => {
-      if (!items) return null
+      if (!items) return null;
 
-      return items.map((item, index) => getItem(item, index, parentItem))
-   }
+      return items.map((item, index) => getItem(item, index, parentItem));
+   };
 
    return (
       <StyledSidebarWrapper>
          <StyledSidebar {...propsWithTheme} collapsed={collapsed}>
             {header && <StyledSidebarHeader>{header}</StyledSidebarHeader>}
-            <StyledSidebarChildren>{Array.isArray(items) ? getItems(items) : null}{children}</StyledSidebarChildren>
-            {footer && <StyledSidebarFooter>{Array.isArray(footer) ? getItems(footer) : footer}</StyledSidebarFooter>}
+            <StyledSidebarChildren>
+               {Array.isArray(items) ? getItems(items) : null}
+               {children}
+            </StyledSidebarChildren>
+            {footer && (
+               <StyledSidebarFooter>
+                  {Array.isArray(footer) ? getItems(footer) : footer}
+               </StyledSidebarFooter>
+            )}
          </StyledSidebar>
       </StyledSidebarWrapper>
-   )
+   );
 };
 
 Sidebar.propTypes = {
-
    header: PropTypes.node,
 
    /**
@@ -255,11 +276,11 @@ Sidebar.propTypes = {
             items: PropTypes.arrayOf(
                PropTypes.shape({
                   label: PropTypes.string.required,
-                  onClick: PropTypes.func
+                  onClick: PropTypes.func,
                })
-            )
+            ),
          })
-      )
+      ),
    ]),
 
    /**
@@ -275,11 +296,11 @@ Sidebar.propTypes = {
             items: PropTypes.arrayOf(
                PropTypes.shape({
                   label: PropTypes.string.required,
-                  onClick: PropTypes.func
+                  onClick: PropTypes.func,
                })
-            )
+            ),
          })
-      )
+      ),
    ]),
 
    /**
@@ -297,7 +318,7 @@ Sidebar.propTypes = {
     */
    collapsed: PropTypes.bool,
 
-   children: PropTypes.node
+   children: PropTypes.node,
 };
 
 Sidebar.defaultProps = {
